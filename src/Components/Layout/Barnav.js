c49bcb429466b9d43e2c01c608952f3b2e5bf2fe
mako <spiller31@hotmail.com>
2019-11-02 18:31:04 -0600

    BarNav Styles aparece en PageHome

--- src/Components/Layout/Barnav.js
+++ src/Components/Layout/Barnav.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Barnav extends Component {
     render() {
         return (
-            <div>
+            <div className="BarNav">
                 
             </div>
         );
---
+++ src/Components/Pages/PageHome.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import BarNav from '../Layout/Barnav';
+
+class PageHome extends Component {
+    render() {
+        return (
+            <div>
+                <BarNav/>
+            </div>
+        );
+    }
+}
+
+export default PageHome;
